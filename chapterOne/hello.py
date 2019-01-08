631f15c93f021d3c527e71d46d1d6995feb35443
Cristian Morataya <cris.morataya@gmail.com>
2019-01-07 23:25:45 -0600

    Fix syntax in str

--- chapterOne/hello.py
+++ chapterOne/hello.py
@@ -14,4 +14,4 @@ print('What is your age?')      # ask for age
 
 myAge = input()
 
-print('You will be ' + str(int(myAge) + 1 + ' in a year. :('))
+print('You will be ' + str(int(myAge) + 1) + ' in a year. :(')
